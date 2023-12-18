9298f0cedfa83046319b0fcca61fe96f8ce0f6c2
Codenaud <codenaud@gmail.com>
2023-12-18 13:28:15 +0100

    Working on exercice 1

--- src/app/interfaces/isteps.ts
+++ src/app/interfaces/isteps.ts
@@ -1,2 +1,7 @@
 export interface Isteps {
+  id: number;
+  title: string;
+  description: string;
+  img: string;
+  bgcolor: string;
 }
--- src/app/services/steps.service.ts
+++ src/app/services/steps.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Isteps } from '../interfaces/isteps';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StepsService {
-
-  constructor() { }
+  constructor() {}
+  getData() {
+    const data: Isteps = {
+      id: 1,
+      title: 'tiutlo 1',
+      description: 'decripcion 1',
+      img: 'string',
+      bgcolor: 'string',
+    };
+  }
 }
